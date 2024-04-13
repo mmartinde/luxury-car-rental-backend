5ff2bde6fc93008e5c34d707b8032a96b3c3e23f
arteaga137 <francisco.arteaga137@gmail.com>
2024-04-13 13:34:42 +0200

    añade userSchema

---
+++ models/user.model.js
@@ -0,0 +1,46 @@
+//Importaciones
+const mongoose = require("mongoose");
+
+//Definición de schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  license: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  Role: {
+    type: String,
+    required: true,
+    enum: ["admin", "user"], // Restringe los roles a estos valores específicos
+    default: "user",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+//Exporta el modelo
+module.exports = User;
