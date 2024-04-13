cf6cd0554d281bdcd343d048642e74790bcfd187
arteaga137 <francisco.arteaga137@gmail.com>
2024-04-13 12:34:45 +0200

    arregla estructura index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+
 // #region CONFIGURACION DE LIBRERIAS
 app.use(express.json());
+
 // #region DEFINICION DE VARIABLES
 const PORT = process.env.PORT || 3000;
 const databaseURL = process.env.MONGODB; //Modificar a la cadena de conexión de MongoDB en el archivo .env
@@ -15,6 +17,7 @@ mongoose
   .catch((err) => console.log(err));
 
 // #region RUTAS
+
 
 //#region SERVIDOR
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
