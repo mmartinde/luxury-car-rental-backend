5b0909622e34bc53d8c7dbe044e1654df3c332e3
urachiche103 <urachiche103@gmail.com>
2024-04-16 20:29:52 +0200

    rent routes terminado

--- routes/rent.routes.js
+++ routes/rent.routes.js
@@ -10,17 +10,23 @@ const {
     deleteRentCar,
 } = require("../controllers/rent.controller");
 
+const {
+    isAuth
+} = require("../middlewares/user.middleware")
+
 //obtiene todos los coches alquilados
-router.get("/", getAllRentCars);
+router.get("/", isAuth, getAllRentCars);
 
 //obtiene coches alquiados por id
 router.get("/:id", getRentCarById);
 
 //crea nuevo coche alquilado
-router.post("/", createRentCar);
+router.post("/", isAuth, createRentCar);
 
 //actualiza coche alquulado por id
-router.put("/:id", updateRentCar);
+router.put("/:id", isAuth, updateRentCar);
 
 //elimina coche alquilado por id
-router.delete("/:id", deleteRentCar);
+router.delete("/:id", isAuth, deleteRentCar);
+
+module.exports = router
