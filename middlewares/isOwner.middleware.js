6b5a9c8e2190234c2ef9b2493dd83e681f6c53ae
Francisco Arteaga <93606979+arteaga137@users.noreply.github.com>
2024-04-18 21:01:26 +0200

    ajusta logica de middleware

--- middlewares/isOwner.middleware.js
+++ middlewares/isOwner.middleware.js
@@ -3,24 +3,31 @@ const Rent = require("../models/rent.model");
 Rent = require("../models/rent.model");
 
 const checkOwnership = async (req, res, next) => {
-  try {
-    const userId = req.user.userId;
-    const rentId = req.params.id;
+  // Verificar si el token está presente en la consulta de la solicitud
+  if (req.query.token) {
+    try {
+      // Intentar verificar el token con la clave secreta del entorno
+      const result = jwt.verify(req.query.token, process.env.JWT_SECRET);
+      const userId = result._id; // Deberia venir en el token?
+      const rentId = req.params.id;
 
-    const rent = await await Rent.findById(rentId);
+      const rent = await await Rent.findById(rentId);
 
-    if (!rent) {
-      return res.status(404).json({ msg: "Rent not found" });
+      if (!rent) {
+        return res.status(404).json({ msg: "Rent not found" });
+      }
+
+      if (rent.userId.toString() !== userId.toString()) {
+        return res.status(403).json({ msg: "Unauthorized access" });
+      }
+
+      next();
+    } catch (error) {
+      console.error("Error checking rent ownership: ", error);
+      res.status(500).json({ msg: "Internal server error" });
     }
-
-    if (rent.userId.toString() !== userId.toString()) {
-      return res.status(403).json({ msg: "Unauthorized access" });
-    }
-
-    next();
-  } catch (error) {
-    console.error("Error checking rent ownership: ", error);
-    res.status(500).json({ msg: "Internal server error" });
+  } else {
+    res.status(400).json({ msg: "Token not provided" });
   }
 };
 
