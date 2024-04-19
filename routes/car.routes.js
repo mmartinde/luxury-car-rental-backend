3732ad535778b025ed095d0c3043d40223202a55
urachiche103 <urachiche103@gmail.com>
2024-04-19 19:01:18 +0200

    car & rent routes prueba

--- routes/car.routes.js
+++ routes/car.routes.js
@@ -11,20 +11,113 @@ const {
 } = require("../controllers/car.controller");
 const { isAuth } = require("../middlewares/isAuth.middleware");
 const { isAdmin } = require("../middlewares/permissions.middleware");
+const { checkOwnership } = require("../middlewares/isOwner.middleware.js");
 
 //obtiene todos los coches
-router.get("/", getAllCars);
+//router.get("/", getAllCars);
+
+//#region GET
+//Obtiene todos los coches
+router.get("/", async (req, res) => {
+  try {
+    const cars = await getAllCars();
+    res.json(cars);
+  } catch (error) {
+    console.error("Could not get all cars:", error);
+    res.status(500).json({ msg: "Internal error" });
+  }
+})
 
 //obtiene coche por id
-router.get("/:id", getCarById);
+//router.get("/:id", getCarById);
 
+router.get("/:id", isAuth, async (req, res) => {
+  try {
+    const foundCar = await getCarById(req.params.id);
+    if (foundCar) {
+      res.json(foundCar);
+    } else {
+      res.status(404).json({ msg: "Error: Car not found" });
+    }
+  } catch (error) {
+    console.error("Could not find car id:", error);
+    res.status(500).json({ msg: "Internal error" });
+  }
+})
+//#endregion
+
+//#region POST
 //crea nuevo coche
-router.post("/", isAuth, isAdmin, createCar);
+//router.post("/", isAuth, isAdmin, createCar);
 
+router.post("/", isAdmin, async (req, res) => {
+  try {
+    const newCar = await createCar(
+      req.body.make?.trim(),
+      req.body.model?.trim(),
+      req.body.plate?.trim(),
+      req.body.year,
+      req.body.hp,
+      req.body.cc,
+      req.body.colour?.trim(),
+      req.body.seats,
+      req.body.price,
+      req.body.transmission?.trim(),
+      req.body.description?.trim()
+    )
+    res.json({ msg: "New car created successfully" });
+  } catch (error) {
+    console.error("Could not create new car:", error);
+    res.status(500).json({ msg: "Internal error" });
+  }
+})
+//#endregion
+
+//#region PUT
 //actualiza coche por id
-router.put("/:id", isAuth, isAdmin, updateCar);
+//router.put("/:id", isAuth, isAdmin, updateCar);
 
+router.put("/:id", isAdmin, async (req, res) => {
+  try {
+    const updatedCar = await updateCar(
+      req.params.id,
+      req.body.make?.trim(),
+      req.body.model?.trim(),
+      req.body.plate?.trim(),
+      req.body.year,
+      req.body.hp,
+      req.body.cc,
+      req.body.colour?.trim(),
+      req.body.seats,
+      req.body.price,
+      req.body.transmission?.trim(),
+      req.body.description?.trim()
+    )
+    res.json(updatedCar);
+  } catch (error) {
+    console.error("Could not update car:", error);
+    res.status(500).json({ msg: "Internal error" });
+  }
+})
+//#endregion
+
+//#region DELETE
 //elimina coche por id
-router.delete("/:id", isAuth, isAdmin, deleteCar);
+//router.delete("/:id", isAuth, isAdmin, deleteCar);
+
+router.delete("/:id", isAdmin, async (req, res) => {
+  try {
+    const deletedCar = await deleteCar(req.params.id);
+    if (deletedCar) {
+      res.json({ msg: "Car deleted" });
+    } else {
+      res.status(404).json({ msg: "Car not found" });
+    }
+  } catch (error) {
+    console.error("Could not delete car:", error);
+    res.status(500).json({ msg: "Internal error" });
+  }
+})
+//#endregion
 
 module.exports = router;
--- routes/rent.routes.js
+++ routes/rent.routes.js
@@ -13,20 +13,103 @@ const {
 } = require("../controllers/rent.controller");
 const { isAuth } = require("../middlewares/isAuth.middleware");
 const { isAdmin } = require("../middlewares/permissions.middleware");
+const { checkOwnership } = require("../middlewares/isOwner.middleware.js");
 
-//obtiene todos los coches alquilados
-router.get("/", isAuth, isAdmin, getAllRentCars); // TODO: Revisar necesidad del controlador (getAllRentCars)
+//Obtiene todos los coches alquilados
+//router.get("/", isAuth, isAdmin, getAllRentCars); TODO: Revisar necesidad del controlador (getAllRentCars)
 
-//obtiene coches alquiados por id
-router.get("/:id", isAuth, getRentCarById);
+//#region GET
+//Obtiene todos los coches alquilados por el usuario
+router.get("/", isAuth, checkOwnership, async (req, res) => {
+  try {
+    const rentCars = await getAllRentCars();
+    res.json(rentCars);
+  } catch (error) {
+    console.error("Could not get all rent cars:", error);
+    res.status(500).json({ msg: "Internal error" });
+  }
+})
 
-//crea nuevo coche alquilado
-router.post("/", isAuth, isAdmin, createRentCar);
+//Obtiene los coches alquilados por id
+//router.get("/:id", isAuth, getRentCarById);
 
-//actualiza coche alquulado por id
-router.put("/:id", isAuth, isAdmin, updateRentCar);
+router.get("/:id", isAuth, checkOwnership, async (req, res) => {
+  try {
+    const foundRentCar = await getRentCarById(req.params.id);
+    if (foundRentCar) {
+      res.json(foundRentCar);
+    } else {
+      res.status(404).json({ msg: "Error: Rent car not found" });
+    }
+  } catch (error) {
+    console.error("Could not find rent car id:", error);
+    res.status(500).json({ msg: "Internal error" });
+  }
+})
+//#endregion
 
+//#region POST
+//Crea nuevo coche alquilado
+//router.post("/", isAuth, isAdmin, createRentCar);
+
+router.post("/", async (req, res) => {
+  try {
+    const newRentCar = await createRentCar(
+      req.body.car,
+      req.body.user,
+      req.body.dateIn,
+      req.body.dateOut,
+      req.body.price,
+      req.body.status
+    )
+    res.json({ msg: "New rent car created successfully" });
+  } catch (error) {
+    console.error("Could not create rent car:", error);
+    res.status(500).json({ msg: "Internal error" });
+  }
+})
+//#endregion
+
+//#region PUT
+//actualiza el coche alquilado por id
+//router.put("/:id", isAuth, isAdmin, updateRentCar);
+
+router.put("/:id", isAuth, checkOwnership, async (req, res) => {
+  try {
+    const updatedRentCar = await updateRentCar(
+      req.params.id,
+      req.body.car,
+      req.body.user,
+      req.body.dateIn,
+      req.body.dateOut,
+      req.body.price,
+      req.body.status
+    )
+    res.json(updatedRentCar);
+  } catch (error) {
+    console.error("Could not update rent car:", error);
+    res.status(500).json({ msg: "Internal error" });
+  }
+})
+//#endregion
+
+//#region DELETE
 //elimina coche alquilado por id
-router.delete("/:id", isAuth, isAdmin, deleteRentCar);
+//router.delete("/:id", isAuth, isAdmin, deleteRentCar);
+
+router.delete("/:id", isAdmin, async (req, res) => {
+  try {
+    const deletedRentCar = await deleteRentCar(req.params.id);
+    if (deletedRentCar) {
+      res.json({ msg: "Rent car deleted" });
+    } else {
+      res.status(404).json({ msg: "Rent car not found" });
+    }
+  } catch (error) {
+    console.error("Could not delete rent car:", error);
+    res.status(500).json({ msg: "Internal error" });
+  }
+})
+//#endregion
 
 module.exports = router;
