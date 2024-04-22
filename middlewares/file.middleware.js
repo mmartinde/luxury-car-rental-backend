4224a5c28d06a42f2850d48537e0824a6a8952c8
Francisco Arteaga <93606979+arteaga137@users.noreply.github.com>
2024-04-22 21:27:30 +0200

    corrige rutas

--- middlewares/file.middleware.js
+++ middlewares/file.middleware.js
@@ -1,54 +1,65 @@
 // Import cloudinary library
-require('dotenv').config();
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs')
-const cloudinary = require('cloudinary').v2;
+require("dotenv").config();
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const cloudinary = require("cloudinary").v2;
 
 // Coudinary config
-cloudinary.config({ 
-    cloud_name: 'urachiche103',
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure: true,
+cloudinary.config({
+  cloud_name: "urachiche103",
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
 const storage = multer.diskStorage({
-    filename: (req, file, cb) => {
-        cd(null, `${Date.now()}-${file.originalname}`);
-    },
-    destination : (req, file, cb) => {
-        cd(null, path.join(__dirname, '../temp/uploads'))
-    },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../temp/uploads"));
+  },
 });
 
-const VALID_FILE_TYPES = ['image/png', 'image/jpg', 'image/jpeg'];
+const VALID_FILE_TYPES = ["image/png", "image/jpg", "image/jpeg"];
 
 const fileFilter = (req, res, cb) => {
-    if (!VALID_FILE_TYPES.includes(file.mimetype)) {
-        cb(new Error('Invalid file type'));
-    } else {
-        cd(null, true);
-    }
+  if (!VALID_FILE_TYPES.includes(file.mimetype)) {
+    cb(new Error("Invalid file type"));
+  } else {
+    cb(null, true);
+  }
 };
 
-const ulpload = multer({
-    storage,
-    fileFilter,
+const upload = multer({
+  storage,
+  fileFilter,
 });
 
 const uploadToCloudinary = async (req, res, next) => {
     if (req.file) {
-        try {
-            const filePath = req.file.path;
-            const image = await cloudinary.uploader.upload(filePath);
-            await fs.unlinkSync(filePath);
-            req.file_url = image.secure_url;
-            next();
-        } catch(error) {
-            return console.log(error)
-        }
+      try {
+        const filePath = req.file.path;
+        const picture = await cloudinary.uploader.upload(filePath);
+  
+        // Asynchronously delete the file after uploading to Cloudinary
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            console.error("Failed to delete temporary file:", err);
+            // Optional: You may choose to still allow the process to proceed even if file deletion fails
+          }
+        });
+  
+        req.file_url = picture.secure_url;
+        next();
+      } catch (error) {
+        console.error("Error uploading to Cloudinary:", error);
+        res.status(500).json({ message: "Failed to upload image" });
+      }
+    } else {
+      res.status(400).json({ message: "No file provided" });
     }
-};
+  };
 
-module.exports = {uploadToCloudinary};
+module.exports = { uploadToCloudinary };
